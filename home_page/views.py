f9e35398a8ba76b48a115490e8cd4bfb7431e3f5
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-11-08 11:09:47 +0700

    OnlineShop -> Changes: - added some changes

--- home_page/views.py
+++ home_page/views.py
@@ -5,13 +5,5 @@ from main_page.models import Menu
 def home_page(request):
     menu = Menu.objects.all()
 
-    cart_prods_counter = request.user.shoppingcart_set.all()
-    favourite_prods_counter = request.user.favourite_set.all()
-    compare_prods_counter = request.user.compare_set.all()
-
     return render(request, 'home_page/home_page.html',
-                  {'menu': menu,
-                   'cart_prods_counter': cart_prods_counter,
-                   'favourite_prods_counter': favourite_prods_counter,
-                   'compare_prods_counter': compare_prods_counter,
-                   })
+                  {'menu': menu})
